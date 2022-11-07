ae088d5d0cd34d15782b78f1d869c47ec1cfee46
Vitor Augusto <95655990+VitorAam@users.noreply.github.com>
2022-11-07 11:08:07 -0300

    feat: implementação de testes para react-router-dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { Home } from './pages/Home/Home.jsx'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <main>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
+    </main>
+    </BrowserRouter>
   </React.StrictMode>
 )
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,7 @@
+export function Home() {
+    return (
+        <div>
+            <h2>Testando rotas</h2>
+        </div>
+    )
+}
