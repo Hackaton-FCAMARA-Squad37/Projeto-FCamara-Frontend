d6771a3a5d3504f59f6880fd6a60eb76ed0e4b9f
Vitor Augusto <95655990+VitorAam@users.noreply.github.com>
2022-11-10 11:11:37 -0300

    fix: correção de merge

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,6 @@ import '../src/style/reset.css'
 import { Header } from './components/Header/Header.jsx'
 import { Footer } from './components/Footer/Footer.jsx'
 
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
