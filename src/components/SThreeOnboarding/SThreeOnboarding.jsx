0ba72d78eaea2d4d45d9476501c7651ed6d4c9a2
Vitor Augusto <95655990+VitorAam@users.noreply.github.com>
2022-11-10 15:35:00 -0300

    feat: implementação da seção 3 do onboarding

---
+++ src/components/SThreeOnboarding/SThreeOnboarding.jsx
@@ -0,0 +1,63 @@
+import { Box, Typography, useMediaQuery } from "@mui/material"
+import logoUXUI from '../../assets/uxuiLogo.png'
+import logoQA from '../../assets/qaLogo.png'
+import logoDEV from '../../assets/devLogo.png'
+
+export const SThreeOnboarding = () => {
+    const mobile = useMediaQuery('(max-width:768px)')
+
+    return (
+        <Box bgcolor='coffee.main' color='water.main' textAlign='center' sx={{
+            padding:'0rem 3rem 3rem 3rem'
+        }}>
+            <Typography fontWeight='600' fontSize={mobile?'1rem':'1.5rem'} mb={mobile? '2rem':'4rem'}>
+                Você pode escolher qualquer uma das áreas disponíveis para estudar
+            </Typography>
+            <Box sx={{
+                display:'flex',
+                flexDirection: `${mobile?'column':'row'}`,
+                justifyContent:'space-between',
+                paddingLeft:`${mobile?'50px':''}`
+            }}>
+                <Box
+                sx={
+                {
+                    backgroundImage: `url(${logoUXUI})`,
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    width: `${mobile?'21.375rem':'22.688rem'}`,
+                    height: `${mobile?'17.5rem':'22rem'}`,
+                    borderRadius: '8px',
+                    marginRight:`${mobile?'auto':'1.5rem'}`,
+                    marginBottom: `${mobile?'1rem':''}`,
+                    marginLeft:`${mobile?'auto':'0'}`,
+                    alignContent:'center'
+                }}/>
+                <Box
+                sx={
+                {
+                    backgroundImage: `url(${logoQA})`,
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    width: `${mobile?'21.375rem':'22.688rem'}`,
+                    height: `${mobile?'17.5rem':'22rem'}`,
+                    borderRadius: '8px',
+                    marginRight:`${mobile?'auto':'1.5rem'}`,
+                    marginBottom: `${mobile?'1rem':''}`,
+                    marginLeft:`${mobile?'auto':'0'}`
+                }}/>
+                <Box
+                sx={
+                {
+                    backgroundImage: `url(${logoDEV})`,
+                    backgroundSize: 'contain',
+                    backgroundRepeat: 'no-repeat',
+                    width: `${mobile?'21.375rem':'22.688rem'}`,
+                    height: `${mobile?'17.5rem':'22rem'}`,
+                    borderRadius: '8px',
+                    margin:`${mobile?'auto':''}`
+                }}/>
+            </Box>
+        </Box>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,15 @@
 import { Box} from "@mui/material";
 import { SOneOnboarding } from "../../components/SOneOnboarding/SOneOnobarding";
 import { STwoOnboarding } from "../../components/STwoOnboarding/STwoOnboarding";
+import { SThreeOnboarding } from "../../components/SThreeOnboarding/SThreeOnboarding";
+
 
 export function Home() {
     return (
         <Box>
             <SOneOnboarding/>
             <STwoOnboarding/>
+            <SThreeOnboarding/>
         </Box>
     )
 }
