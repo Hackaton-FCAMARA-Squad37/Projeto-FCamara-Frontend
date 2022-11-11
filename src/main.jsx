25527fba6755e0bff114cc57d3122887975473a4
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2022-11-11 17:29:30 -0300

    feat: criando contexto de usuário e implementando no login

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { ThemeProvider } from '@mui/material'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home/Home.jsx'
-import { theme } from './style/theme.js'
-import '../src/style/reset.css'
-import { Header } from './components/Header/Header.jsx'
-import { Footer } from './components/Footer/Footer.jsx'
-import { Login } from './pages/Login/Login.jsx'
-import { SignIn } from './pages/SignIn/SigIn.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home/Home.jsx";
+import { theme } from "./style/theme.js";
+import "../src/style/reset.css";
+import { Header } from "./components/Header/Header.jsx";
+import { Footer } from "./components/Footer/Footer.jsx";
+import { Login } from "./pages/Login/Login.jsx";
+import { SignIn } from "./pages/SignIn/SigIn.jsx";
+import { UserContextProvider } from "../src/context/user/UserContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <UserContextProvider>
         <BrowserRouter>
           <header>
-            <Header/>
+            <Header />
           </header>
           <main>
             <Routes>
-              <Route path='/' element={<Home/>}/>
-              <Route path='/login' element={<Login/>}/>
-              <Route path='/signin' element={<SignIn/>}/>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/signin" element={<SignIn />} />
             </Routes>
           </main>
           <footer>
-            <Footer/>
+            <Footer />
           </footer>
         </BrowserRouter>
+      </UserContextProvider>
     </ThemeProvider>
   </React.StrictMode>
-)
+);
