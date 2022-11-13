48c180de93e83cfde5b7b4cd49f0d5f81e62746f
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2022-11-12 22:00:56 -0300

    feat: cadastrar o usuário na trilha desejada

--- src/pages/UxuiPage/UxuiPage.jsx
+++ src/pages/UxuiPage/UxuiPage.jsx
@@ -45,16 +45,31 @@ export function UxuiPage() {
             sx={{
               fontSize: `${mobile ? "1rem" : "1.5rem"}`,
               paddingLeft: `${mobile ? "0.5rem" : "1.375rem"}`,
-              fontWeight:`${mobile?'':'600'}`
+              fontWeight: `${mobile ? "" : "600"}`,
             }}
           >
             Voltar
           </Typography>
         </Box>
       </Link>
-      <CardTrail imagem={semear} titulo='Semear' descricao='Semear é o primeiro nível da trilha, nela você vai aprender assuntos básicos que irão te preparar para os próximos níveis. Se você está iniciando seus estudos em tecnologia, essa trilha é para você'/>
-      <CardTrail imagem={regar} titulo='Regar' descricao='Regar é o segundo nível da trilha, nela há conteúdos considerados intermediários pela nossa consultroria. Se você já possui algum tempo estudando tecnologia, essa trilha te ajudará'/>
-      <CardTrail imagem={coletar} titulo='Coletar' descricao='Coletar  é o terceiro e último nível da trilha, nela têm conteúdos mais avançados. Se você busca por assuntos mais específicos ou linguagens de programação pouco usadas, essa trilha te auxiliará'/>
+      <CardTrail
+        temaId="3"
+        imagem={semear}
+        titulo="Semear"
+        descricao="Semear é o primeiro nível da trilha, nela você vai aprender assuntos básicos que irão te preparar para os próximos níveis. Se você está iniciando seus estudos em tecnologia, essa trilha é para você"
+      />
+      <CardTrail
+        temaId="4"
+        imagem={regar}
+        titulo="Regar"
+        descricao="Regar é o segundo nível da trilha, nela há conteúdos considerados intermediários pela nossa consultroria. Se você já possui algum tempo estudando tecnologia, essa trilha te ajudará"
+      />
+      <CardTrail
+        temaId="5"
+        imagem={coletar}
+        titulo="Coletar"
+        descricao="Coletar  é o terceiro e último nível da trilha, nela têm conteúdos mais avançados. Se você busca por assuntos mais específicos ou linguagens de programação pouco usadas, essa trilha te auxiliará"
+      />
     </Box>
   );
 }
