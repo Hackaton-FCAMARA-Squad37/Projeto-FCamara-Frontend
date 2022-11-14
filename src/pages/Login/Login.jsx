14ada33dd7fcd4ddaae5b9caa337ce41edc2a38c
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2022-11-14 03:15:06 -0300

    feat: estilização do login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,80 +1,123 @@
 import * as React from "react";
 import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { Link } from "react-router-dom";
 import { useContext } from "react";
 import UserContext from "../../context/user/UserContext";
-import useUserState from "../../hook/useUserState";
+import { InputCustomized } from "../../components/InputCustomized/InputCustomized";
+import { useRef } from "react";
+import { useForm } from "react-hook-form";
+import { useMediaQuery } from "@mui/material";
+import logoOrangeJuice from "../../assets/logoOrangeJuice.png";
 
 export function Login() {
-  const { login, usuario } = useContext(UserContext);
-  const loged = useUserState();
-  const logado = loged.id;
+  const { login } = useContext(UserContext);
+  const emailRef = useRef(null);
+  const senhaRef = useRef(null);
+  const mobile = useMediaQuery("(max-width:768px)");
 
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    const data = new FormData(event.currentTarget);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const handleSubmitLogin = async (data) => {
     console.log({
-      email: data.get("email"),
-      password: data.get("password"),
+      email: data.email,
+      password: data.senha,
     });
 
-    await login(data.get("email"), data.get("password"));
+    await login(data.email, data.senha);
   };
 
   return (
-    <Container component="main" maxWidth="xs">
+    <Container
+      component="div"
+      maxWidth="100%"
+      sx={{
+        backgroundColor: "coffee.main",
+        padding: "4rem 1.5rem",
+        color: "white",
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+      }}
+    >
       <Box
         sx={{
-          marginTop: 8,
           display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
+          flexDirection: `${mobile ? "column" : "row"}`,
+          gap: `${mobile ? "1.5rem" : "7.5rem"}`,
+          alignItems: `${mobile ? "" : "center"}`,
         }}
       >
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
+        <Box>
+          {mobile ? (
+            ""
+          ) : (
+            <Box
+              sx={{
+                backgroundImage: `url(${logoOrangeJuice})`,
+                backgroundRepeat: "no-repeat",
+                backgroundSize: "contain",
+                width: "100px",
+                height: "100px",
+              }}
+            />
+          )}
+
+          <Typography component="h1" variant="h5" sx={{ fontWeight: 600 }}>
+            Olá, acesse sua conta
+          </Typography>
+        </Box>
+        <Box
+          component="form"
+          onSubmit={handleSubmit(handleSubmitLogin)}
+          sx={{
+            border: "1px solid",
+            borderColor: "milkshake.main",
+            borderRadius: "8px",
+            padding: `${mobile ? "1.5rem 1rem" : "3.5rem 3rem"}`,
+          }}
+        >
+          <InputCustomized
+            tipo="email"
+            texto="Seu e-mail"
             id="email"
-            label="Email"
-            name="email"
-            autoComplete="email"
-            autoFocus
+            placeholder="exemplo@email.com"
+            reference={emailRef}
+            register={register}
           />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Senha"
-            type="password"
-            id="password"
-            autoComplete="current-password"
+          {errors.email?.type === "required" && <p>Email é obrigatório.</p>}
+          <InputCustomized
+            tipo="senha"
+            texto="Sua senha"
+            id="senha"
+            placeholder="*********"
+            reference={senhaRef}
+            register={register}
           />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="primary" />}
-            label="Remember me"
-          />
-              
-            <Button
+          {errors.senha?.type === "required" && <p>Senha é obrigatória.</p>}
+
+          <Button
             type="submit"
             fullWidth
             variant="contained"
-            sx={{ mt: 3, mb: 2 }}
-            >
-            Logar
-            </Button>
-          <Link to="/signin">Cadastre-se</Link>
+            sx={{ mt: 3, mb: 2, padding: "0.75rem 0", fontWeight: 600 }}
+          >
+            Entrar
+          </Button>
+          <Typography
+            sx={{ textAlign: "center", marginTop: "2rem", marginBottom: "0" }}
+          >
+            Não tem uma conta?{" "}
+            <Link to="/signin" style={{ color: "white", fontWeight: 600 }}>
+              Cadastre-se
+            </Link>
+          </Typography>
         </Box>
       </Box>
     </Container>
