656d089afd8ac8d37de92dd5ae29e8dfad2fb5a9
Guilherme Silveira Coutinho <guisilveira.cout@gmail.com>
2022-11-14 21:13:37 -0300

    fix: corrigindo bugs

--- src/style/theme.js
+++ src/style/theme.js
@@ -1,24 +1,25 @@
-import { createTheme } from '@mui/material'; 
-import { stateAndNeutral } from './colors'
+import { createTheme } from "@mui/material";
+import { stateAndNeutral } from "./colors";
 
 export const theme = createTheme({
   palette: {
     primary: {
-      main: '#00C09B'
+      main: "#00C09B",
     },
     secondary: {
-      main: '#8C1AD1',
-      strawberryJuice: '#FA2882'
+      main: "#8C1AD1",
+      strawberryJuice: "#FA2882",
     },
     ...stateAndNeutral,
     gradients: {
-      mixOfFlavors: 'linear-gradient(90deg, rgba(253,106,192,1) 0%, rgba(187,0,225,1) 35%, rgba(81,64,214,1) 60%, rgba(0,192,155,1) 100%)'
-    }
+      mixOfFlavors:
+        "linear-gradient(90deg, rgba(253,106,192,1) 0%, rgba(187,0,225,1) 35%, rgba(81,64,214,1) 60%, rgba(0,192,155,1) 100%)",
+    },
   },
   typography: {
     fontFamily: `'Raleway', sans-serif`,
     button: {
-      textTransform: 'none'
-    }
-  }
+      textTransform: "none",
+    },
+  },
 });
