c3cb801a4356eca02638514097e5e1246b1d8980
Vitor Augusto <95655990+VitorAam@users.noreply.github.com>
2022-11-15 21:45:34 -0300

    fix: correção de comportamento

--- src/components/CardConteudo/CardConteudo.jsx
+++ src/components/CardConteudo/CardConteudo.jsx
@@ -95,7 +95,7 @@ export const CardConteudo = (props) => {
         )}
         {admin.isAdmin?<Box
           onClick={() => {
-            console.log("Deleta conteudo");
+            console.log(excluirConteudo);
           }}
           sx={{
             border: "1px solid #8A0300",
